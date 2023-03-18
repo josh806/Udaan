3409552573a0423a8607814b25a3e4afa169a4c3
josh <joshuathomas18@atlassian.com>
2023-03-18 11:38:06 +0100

    Feat:Client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import Phaser from "./Phaser/Phaser";
+import "./App.css";
+import { useState } from "react";
+import VideoChat from "./Components/VideoChat";
+
+
+
+function App() {
+  // example of chat component on top of phaser
+  // to be implemented with redux.
+const [chat, setChat] = useState(false);
+  const scene = Phaser;
+  let ui: JSX.Element
+
+  return (
+    <>
+      {chat ? (
+        <VideoChat/>
+      ) : (<button className="open_chat" onClick={() => setChat(!chat)}>Show chat</button>)}
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Phaser/Phaser.tsx
@@ -0,0 +1,20 @@
+import Phaser from "phaser";
+
+import HelloWorldScene from "./HelloWorldScene";
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: "app",
+  width: window.innerWidth,
+  height: window.innerHeight,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug :false
+    },
+  },
+  scene: [HelloWorldScene],
+};
+
+export default new Phaser.Game(config);
