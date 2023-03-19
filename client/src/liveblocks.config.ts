9588e086f0ae7e659a5ad7cbe8ed34154512fd76
YacineKadri <ya.kadri@gmail.com>
2023-03-19 23:13:04 +0100

    feat: begun  implementation of liveblocks elements

---
+++ client/src/liveblocks.config.ts
@@ -0,0 +1,18 @@
+import { createClient, LiveObject } from "@liveblocks/client";
+import { createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  publicApiKey:
+    "pk_dev_PtrABg2QEleD_tDZgGAGFXQR22oVq5HNnrYimQRojigKsm9USlNuhETjOfYpuGZp",
+});
+
+type Presence = {
+  cursor: { x: number; y: number } | null;
+};
+
+type Storage = {
+    student: LiveObject<{ firstName: string; lastName: string }>;
+  };
+
+export const { RoomProvider, useOthers, useUpdateMyPresence, useStorage, useMutation, useUndo, useRedo } =
+  createRoomContext<Presence, Storage>(client);
