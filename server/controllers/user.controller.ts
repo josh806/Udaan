d82361dc6c4157e670a0c3ac7d2200fc2a98a7fa
valgaug <valentin.gaugain@hotmail.fr>
2023-03-20 13:54:55 +0100

    feat: .env added

---
+++ server/controllers/user.controller.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {
+  }
+};
+
+export { createUser };
---
+++ server/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const router_1 = __importDefault(require("./routes/router"));
+const PORT = process.env.PORT || 3001;
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(router_1.default);
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
