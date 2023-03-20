27244e6414aa6b78e03cf4bd9ae52d7f2834feec
valgaug <valentin.gaugain@hotmail.fr>
2023-03-20 15:40:43 +0100

    fix: eS Lint fix

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,11 +1,18 @@
 import { PrismaClient } from '@prisma/client';
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
 const prisma = new PrismaClient();
 
 const createUser = async (req: Request, res: Response) => {
   try {
+    const createUser = await prisma.user.create({
+      data: req.body,
+    });
+    res.status(200);
+    res.send(createUser);
   } catch (error) {
+    console.log(error);
+    res.status(300);
   }
 };
 
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,12 @@
-require('dotenv').config();
-import express, { Application } from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
 import cors from 'cors';
 import router from './routes/router';
 
 const PORT = process.env.PORT || 3001;
 
-const app: Application = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
