b1a81a334558779a150792ab4e6111f81869b004
YacineKadri <ya.kadri@gmail.com>
2023-03-21 15:58:45 +0100

    feat: toolbar on the whiteboard

--- client/src/Components/InteractiveWhiteboard/InteractiveWhiteboard.tsx
+++ client/src/Components/InteractiveWhiteboard/InteractiveWhiteboard.tsx
@@ -13,12 +13,57 @@ const InteractiveWhiteboard = () => {
         appIdentifier: "F32XkMcZEe2safl2pxmVng/oPm-ru64AhNC1g",
         region: "us-sv",
       });
-      const room = await whiteWebSdk.joinRoom({
-        uuid: roomJSON.uuid,
-        uid: "123",
-        roomToken: roomToken,
-      });
-      setRoom(room);
+      const room = await whiteWebSdk
+        .joinRoom({
+          uuid: roomJSON.uuid,
+          uid: "123",
+          roomToken: roomToken,
+        })
+        .then((room) => {
+          var toolbar = document.getElementById("toolbar");
+          console.log(toolbar);
+          var toolNames = [
+            "clicker",
+            "selector",
+            "rectangle",
+            "eraser",
+            "text",
+            "arrow",
+            "ellipse",
+            "hand",
+            "laserPointer",
+            "shape",
+            "straight",
+          ];
+
+          for (var idx in toolNames) {
+            var toolName = toolNames[idx];
+            var btn = document.createElement("BUTTON");
+            btn.setAttribute("id", "btn" + toolName);
+            var t = document.createTextNode(toolName);
+            btn.appendChild(t);
+
+            // Listen for the event of clicking a button.
+            btn.addEventListener("click", function (obj) {
+              var ele = obj.target;
+              // Call the setMemberState method to set the whiteboard tool.
+              room.setMemberState({
+                currentApplianceName: ele.getAttribute("id").substring(3),
+                shapeType: "pentagram",
+                strokeColor: [255, 182, 200],
+                strokeWidth: 12,
+                textSize: 40,
+              });
+            });
+            toolbar.appendChild(btn);
+            console.log(btn.getAttribute("id"));
+          }
+          setRoom(room);
+        })
+        .catch(function (err) {
+          console.error(err);
+        });
+     
     } catch (err) {
       console.error(err);
     }
@@ -28,12 +73,14 @@ const InteractiveWhiteboard = () => {
     <div>
       <button onClick={createRoomAndJoin}>Create Room and Join</button>
       {room && (
-        <RoomWhiteboard
-          room={room}
-          style={{ width: "1000vh", height: "100vh", background: "white" }}
+          <RoomWhiteboard
+            room={room}
+            style={{ width: "1000vh", height: "100vh", background: "white" }}
+          />
+        ) }
+     <div id="toolbar" style={{ background: "black" }}>
 
-        />
-      )}
+     </div>
     </div>
   );
 };
