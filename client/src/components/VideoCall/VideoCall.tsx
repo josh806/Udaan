dc128107c94a1212ce6592461a2ba46ddecb4028
Gareth Malik <ya.kadri@gmail.com>
2023-03-28 15:45:58 +0200

    feat: for merge

--- client/src/components/VideoCall/VideoCall.tsx
+++ client/src/components/VideoCall/VideoCall.tsx
@@ -1,62 +1,63 @@
+
+
 import React, { useEffect, useState } from 'react';
 import Controls from './Controls';
 import Videos from './Videos';
 import './VideoCall.css';
 import { ClientConfig, IAgoraRTCRemoteUser } from 'agora-rtc-sdk-ng';
 import { createClient, createMicrophoneAndCameraTracks } from 'agora-rtc-react';
-import axios from 'axios';
-
-
-
 
 const config: ClientConfig = {
   mode: 'rtc',
   codec: 'vp8',
 };
 
+
+ 
+
 const appId = '982666deb2ab44e7a3ab95555076b864';
+const token: string | null =
+  '007eJxTYNjy+tOSL85PNP7m7fjLlpdgeH3DtAvJK6d9O/2lIMhKq41NgcHSwsjMzCwlNckoMcnEJNU80TgxydIUCAzMzZIszEyEtiumNAQyMmQVbWFiZIBAEJ+HISS1uCQ8syTDKz8rn4EBAF94JKQ=';
 const useClient = createClient(config);
 const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
 
 //VideoComponent for the video call
-export const VideoCall = () => {
-  const [channelName, setChannelName] = useState<string>('');
-  const [tokenGenerated, setTokenGenerated] = useState<string>('');
+const VideoCall = () => {
+  const channelName = 'TestWithJojo';
   const [remoteUsers, setRemoteUsers] = useState<IAgoraRTCRemoteUser[]>([]);
   const [start, setStart] = useState<boolean>(false);
   const client = useClient();
   const { ready, tracks } = useMicrophoneAndCameraTracks();
-  console.log(tokenGenerated, 'hello');
 
   const options =
-{
-  appId: '982666deb2ab44e7a3ab95555076b864',
-
-  channel: channelName,
-
-  token: '',
-  uid: 0,
-  ExpireTime: 60,
-  serverUrl: 'https://agora-token-service-production-4720.up.railway.app' 
-};
-  async function FetchToken()
   {
-    return new Promise(function (resolve)
+    appId: '982666deb2ab44e7a3ab95555076b864',
+  
+    channel: channelName,
+  
+    token: '',
+    uid: 0,
+    ExpireTime: 60,
+    serverUrl: 'https://agora-token-service-production-4720.up.railway.app' 
+  };
+    async function FetchToken()
     {
-      axios.get(options.serverUrl+'/rtc/'+options.channel+'/1/uid/'+options.uid+'/?expiry='+ options.ExpireTime)
-        .then(
-          response =>
+      return new Promise(function (resolve)
+      {
+        axios.get(options.serverUrl+'/rtc/'+options.channel+'/1/uid/'+options.uid+'/?expiry='+ options.ExpireTime)
+          .then(
+            response =>
+            {
+              console.log(response.data.rtcToken);
+              resolve(response.data.rtcToken);
+              setTokenGenerated(response.data.rtcToken);
+            })
+          .catch(error =>
           {
-            console.log(response.data.rtcToken);
-            resolve(response.data.rtcToken);
-            setTokenGenerated(response.data.rtcToken);
-          })
-        .catch(error =>
-        {
-          console.log(error);
-        });
-    });
-  }
+            console.log(error);
+          });
+      });
+    }
 
   useEffect(() => {
     // function to initialise the SDK
@@ -93,7 +94,7 @@ export const VideoCall = () => {
         });
       });
 
-      await client.join(appId, name, tokenGenerated, null);
+      await client.join(appId, name, token, null);
       if (tracks) await client.publish([tracks[0], tracks[1]]);
       setStart(true);
     };
@@ -107,39 +108,39 @@ export const VideoCall = () => {
   return (
     <div className='videocall-container'>
       {ready && tracks && (
-        <Controls client={client} tracks={tracks} setStart={setStart} />)}
+        <Controls client={client} tracks={tracks} setStart={setStart} />
+      )}
       {start && tracks && <Videos users={remoteUsers} tracks={tracks} />}
-      <ChannelForm setInCall={setStart} setChannelName={setChannelName} />
     </div>
   );
 };
 
-
-export const ChannelForm = (props: {
-  setInCall: React.Dispatch<React.SetStateAction<boolean>>;
-  setChannelName: React.Dispatch<React.SetStateAction<string>>;
-}) => {
-  const { setInCall, setChannelName } = props;
-  return (
-    <form className="join">
-      {appId === '' ? (
-        <p style={{ color: 'red' }}>Please enter your Student ID</p>
-      ) : null}
-      <input
-        type="text"
-        placeholder="Enter Channel Name"
-        onChange={(e) => setChannelName(e.target.value)}
-      />
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          FetchToken();
-        }}
-      >
-        Join
-      </button>
-    </form>
-  );
-};
-
+//Form to enter the channel name, to be used for teachers to create class.
+// right now it is for entering the class. need to be refactored
+// const ChannelForm = (props: {
+//   setInCall: React.Dispatch<React.SetStateAction<boolean>>;
+//   setChannelName: React.Dispatch<React.SetStateAction<string>>;
+// }) => {
+//   const { setInCall, setChannelName } = props;
+//   return (
+//     <form className="join">
+//       {appId === '' ? (
+//         <p style={{ color: 'red' }}>Please enter your Student ID</p>
+//       ) : null}
+//       <input
+//         type="text"
+//         placeholder="Enter Channel Name"
+//         onChange={(e) => setChannelName(e.target.value)}
+//       />
+//       <button
+//         onClick={(e) => {
+//           e.preventDefault();
+//           setInCall(true);
+//         }}
+//       >
+//         Join
+//       </button>
+//     </form>
+//   );
+// };
 export default VideoCall;
