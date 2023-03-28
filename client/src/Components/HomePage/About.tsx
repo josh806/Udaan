d795110fd43c3143df87b57130e6fea6c7f65de7
valgaug <valentin.gaugain@hotmail.fr>
2023-03-28 10:58:28 +0200

    fix: home colors

--- client/src/Components/HomePage/About.tsx
+++ client/src/Components/HomePage/About.tsx
@@ -5,21 +5,9 @@ const About = function () {
       <div id = 'About_container'>
         <div className='About_content_container'>
           <h1>About</h1>
-          <h2>What is the game about?</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-            officiis voluptate pariatur reiciendis quis ab, ea placeat
-            voluptatem debitis accusamus minima adipisci quasi temporibus
-            incidunt laborum nulla beatae modi unde!
+          <p>Learning is not always an easy and peaceful journey to reach your achievements. Specially when you're a child. With Udaan we've decided to make your school as fun and interactive as possible so you keep learning with ease in the best conditions.
           </p>
         </div>
-        <h2>What are we solving?</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vel
-          sequi consequuntur voluptas error atque reiciendis dolorem! Repellat
-          excepturi labore delectus, ipsam quae amet vero vel, adipisci alias ea
-          dolorem.
-        </p>
         <div className='About_game_example'>
           <img src='' alt='' />
         </div>
