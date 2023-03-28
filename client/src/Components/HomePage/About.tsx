c3b9c559787c5a2862e5364342af59d7682967eb
valgaug <valentin.gaugain@hotmail.fr>
2023-03-28 10:59:00 +0200

    again

--- client/src/Components/HomePage/About.tsx
+++ client/src/Components/HomePage/About.tsx
@@ -5,7 +5,7 @@ const About = function () {
       <div id = 'About_container'>
         <div className='About_content_container'>
           <h1>About</h1>
-          <p>Learning is not always an easy and peaceful journey to reach your achievements. Specially when you're a child. With Udaan we've decided to make your school as fun and interactive as possible so you keep learning with ease in the best conditions.
+          <p>Learning is not always an easy and peaceful journey to reach your achievements. Specially when you are a child. With Udaan we have decided to make your school as fun and interactive as possible so you keep learning with ease in the best conditions.
           </p>
         </div>
         <div className='About_game_example'>
