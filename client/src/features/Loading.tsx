18cf66f48b707782029d59b29af9be167937749f
josh <joshuathomas18@gmail.com>
2023-03-29 12:40:51 +0200

    Feat: setup loading page

--- client/src/features/Loading.tsx
+++ client/src/features/Loading.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import avatar1_walking from '../assets/avatar1_walking.gif';
-import kyle_walking from '../assets/kyle_walking.gif';
-import avatar2_walking from '../assets/avatar3_walking.gif';
+import avatar1_walking from '../assets/gifs/loading/avatar1_walking.gif';
+import avatar2_walking from '../assets/gifs/loading/avatar2_walking.gif';
+import avatar3_walking from '../assets/gifs/loading/avatar3_walking.gif';
+import './Loading.css';
 const Loading = () => {
   return (
     <>
       <div className='loading_page_background'>
-        <div>Loading</div>
-        <img src={avatar1_walking} alt='' />
-        <img src={kyle_walking} alt='' />
-        <img src={avatar2_walking} alt='' />
+        <div className='loading_details'>
+          <img
+            className='loading_avatar avatar1'
+            src={avatar1_walking}
+            alt=''
+          />
+          <img
+            className='loading_avatar avatar2'
+            src={avatar2_walking}
+            alt=''
+          />
+          <img
+            className='loading_avatar avatar3'
+            src={avatar3_walking}
+            alt=''
+          />
+        </div>
       </div>
     </>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './Home.css';
-import alex from '../assets/reading.gif';
-import mark from '../assets/mark_sitting_left.gif';
-import adam from '../assets/adam_phone.gif';
+import alex from '../assets/gifs/homepage/reading.gif';
+import mark from '../assets/gifs/homepage/mark_sitting_left.gif';
+import adam from '../assets/gifs/homepage/adam_phone.gif';
 import AuthLoginBtn from '../auth/AuthLoginBtn';
 import About from '../components/homepage/About';
 import Features from '../components/homepage/Features';
 import Pricing from '../components/homepage/Pricing';
-import Loading from '../features/Loading';
 
 function Home() {
   const handleClickScroll = (text: string) => {
@@ -29,13 +28,17 @@ function Home() {
       <nav className='Home_nav'>
         <div className='logo'>Logo</div>
         <div className='Home_nav_options'>
-          <li onClick={(e) => handleClickScroll(e.currentTarget.innerText) }>About</li>
-          <li onClick={(e) => handleClickScroll(e.currentTarget.innerText) }>Features</li>
-          <li onClick={(e) => handleClickScroll(e.currentTarget.innerText) }>Pricing</li>
+          <li onClick={(e) => handleClickScroll(e.currentTarget.innerText)}>
+            About
+          </li>
+          <li onClick={(e) => handleClickScroll(e.currentTarget.innerText)}>
+            Features
+          </li>
+          <li onClick={(e) => handleClickScroll(e.currentTarget.innerText)}>
+            Pricing
+          </li>
         </div>
-        <div>
-          {/* <button className='signin'>Sign in</button> */}
-        </div>
+        <div>{/* <button className='signin'>Sign in</button> */}</div>
       </nav>
       <div className='Home_background'>
         <div className='details'>
@@ -58,11 +61,8 @@ function Home() {
         <Features />
         <Pricing />
       </div>
-      <div>
-
-      </div>
+      <div></div>
     </>
-    // <Loading />
   );
 }
 
