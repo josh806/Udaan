ffd6bd99fb43129437091ec9ae492c85856b4c66
josh <joshuathomas18@gmail.com>
2023-03-29 13:55:19 +0200

    Loading page complete

--- client/src/features/Loading.tsx
+++ client/src/features/Loading.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import avatar1_walking from '../assets/gifs/loading/avatar1_walking.gif';
 import avatar2_walking from '../assets/gifs/loading/avatar2_walking.gif';
 import avatar3_walking from '../assets/gifs/loading/avatar3_walking.gif';
+import bus_right from '../assets/gifs/loading/bus_right.gif';
 import './Loading.css';
 const Loading = () => {
   return (
@@ -21,6 +22,11 @@ const Loading = () => {
           <img
             className='loading_avatar avatar3'
             src={avatar3_walking}
+            alt=''
+          />
+          <img
+            className='loading_avatar bus'
+            src={bus_right}
             alt=''
           />
         </div>
